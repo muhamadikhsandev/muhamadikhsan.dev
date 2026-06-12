14c3f31c878bd23f931567118700b512d53e52a0
muhamadikhsandev <muhamadikhsan.dev@gmail.com>
2026-06-12 17:34:30 +0700

    next image hero 7

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,25 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    unoptimized: true,
+    // FIX NEXT IMAGE: Menghapus 'unoptimized: true' agar gambar otomatis dikompres ke WebP & dibuat responsif
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'wknxqscwvlphuwtvbvot.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.simpleicons.org',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
